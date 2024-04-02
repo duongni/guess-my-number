2b4376cf14663e2efef6a5122073a03d03efda96
duongni <133175915+duongni@users.noreply.github.com>
2024-04-02 13:21:38 -0400

    implement checking guessed number logic

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 "use strict";
 
-document.querySelector(".message").textContent = "🎉 Correct Number";
+//define secret number:
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".box").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️ No number!";
+  } else if (guess === 0) {
+    document.querySelector(".message").textContent =
+      "Input number from 1 to 20!";
+  } else if (guess > 20) {
+    document.querySelector(".message").textContent =
+      "Input number from 1 to 20!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  }
+});
